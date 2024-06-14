721286457f6eecb331807e673a916fed05a3084c
Marco A <161896294+MarcoALR@users.noreply.github.com>
2024-06-14 11:02:57 -0300

    Add files via upload

---
+++ Atividade 6 - Programando com Javascript/Conversor de Metros para Centímetros.js
@@ -0,0 +1,5 @@
+function converterParaCentimetros() {
+    var metros = document.getElementById('inputMetros').value;
+    var centimetros = metros * 100;
+    document.getElementById('resultado').innerHTML = metros + " metros é igual a " + centimetros + " centímetros.";
+}
---
+++ Atividade 6 - Programando com Javascript/Meu primeiro programa JavaScript.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerHTML = "Alô mundo";
+console.log("Alô mundo");
+alert("Alô Mundo");
